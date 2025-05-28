19c1a65473089fd82638bf48c0144ffc8fbb3851
Hossam Zakaria Sannad <hosamking2122@gmail.com>
2025-05-28 17:09:42 +0300

    for deploy render

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^s7skalx7yabjgz6lietdhtqi-y$!1(+urzvuzjo-vz@7v&dd@'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
